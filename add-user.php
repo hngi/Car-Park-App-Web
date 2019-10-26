0f32b9955459da5db4360f442aa6bd066e7062b3
Olaniyi Oshunbote <oshunboteo@gmail.com>
2019-10-26 03:51:50 +0100

    tweaking files under task assigned; pay-history and add-account

--- add-user.php
+++ add-user.php
@@ -1,18 +1,16 @@
 <?php
 
-//Block 1
-$user = "1rekMHJ7Mr"; //Enter the user name
-$password = "Huf14jE5rb"; //Enter the password
-$host = "remotemysql.com:3306"; //Enter the host
-$dbase = "1rekMHJ7Mr"; //Enter the database
-$table = "carparks"; //Enter the table name
+$user = "1rekMHJ7Mr";
+$password = "Huf14jE5rb";
+$host = "remotemysql.com:3306";
+$dbase = "1rekMHJ7Mr";
+$table = "add-users";
 
-//Block 2
 $firstname= $_POST['firstName'];
 $lastname= $_POST['lastName'];
+$name= '$firstname '.'$lastname';
 $email= $_POST['email'];
 
-//Block 3
 $connection= mysql_connect ($host, $user, $password);
 if (!$connection)
 {
@@ -20,20 +18,13 @@ die ('Could not connect:' . mysql_error());
 }
 mysql_select_db($database, $connection);
 
-
-//Block 4
 $username_table= mysql_query( "SELECT username FROM $table WHERE username= '$username'" ) 
 or die("SELECT Error: ".mysql_error()); 
 
+mysql_query("INSERT INTO $table (Name, Email) VALUES ('$name', '$email')");
 
-//Block 5
-mysql_query("INSERT INTO $table (column1, column2, column3)
-VALUES (value1, value2, value 3)");
-
-//Block 6
 echo 'You have been added.';
 
-//Block 7
 mysql_close($connection);
 
 ?>
